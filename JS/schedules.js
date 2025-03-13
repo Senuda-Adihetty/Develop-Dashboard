ab96dda4a429320945556cfbe7d685769282c81f
Senuda Damnidu Karunarathne Adihetty <senuadihetty429@gmail.com>
2025-03-13 19:31:18 +0530

    finish

---
+++ JS/schedules.js
@@ -0,0 +1,32 @@
+const Schedules = [
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'LARVEtOER KFIØ2 Drone',
+    productNumber: '36378',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'Ruko FII Pro Drone',
+    productNumber: '49347',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Drone with Camera Drone',
+    productNumber: '96996',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'GPS 4K Drone',
+    productNumber: '22821',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  }
+];
--- JS/script.js
+++ JS/script.js
@@ -15,8 +15,24 @@ closeBtn.addEventListener("click", () => {
 
 //  Change Theme
 themeToggler.addEventListener("click", () => {
-  document.body.classList.toggle('dark-theme-variables');
+  document.body.classList.toggle("dark-theme-variables");
 
   themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
   themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+});
+
+// Fill Table
+Schedules.forEach(Schedules => {
+  const tr = document.createElement('tr');
+  const trContent = `
+                <td>${Schedules.productName}</td>
+                <td>${Schedules.productNumber}</td>
+                <td>${Schedules.paymentStatus}</td>
+                <td class="${Schedules.shipping === 'Declined' ? 'danger' : Schedules.shipping === 'Pending' ? 'warning' : 'primary'}"> ${Schedules.shipping}</td>
+                <td class="primary"> Details </td>`;
+
+tr.innerHTML = trContent;
+document.querySelector('table tbody').appendChild(tr);
+
+
 });
