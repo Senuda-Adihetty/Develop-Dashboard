e937b088d5fd1915674e1b547352a01add3bbc04
Senuda Damnidu Karunarathne Adihetty <senuadihetty429@gmail.com>
2025-03-13 19:45:20 +0530

    finished v.01

--- JS/script.js
+++ JS/script.js
@@ -21,18 +21,37 @@ themeToggler.addEventListener("click", () => {
   themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
 });
 
+// // Fill Table
+// Schedules.forEach(Schedules => {
+//   const tr = document.createElement('tr');
+//   const trContent = `
+//                 <td>${Schedules.productName}</td>
+//                 <td>${Schedules.productNumber}</td>
+//                 <td>${Schedules.paymentStatus}</td>
+//                 <td class="${Schedules.shipping === 'Declined' ? 'danger' : Schedules.shipping === 'Pending' ? 'warning' : 'primary'}"> ${Schedules.shipping}</td>
+//                 <td class="primary"> Details </td>`;
+
+// tr.innerHTML = trContent;
+// document.querySelector('table tbody').appendChild(tr);
+
+
+// });
+
 // Fill Table
-Schedules.forEach(Schedules => {
+Schedules.forEach(schedule => {
   const tr = document.createElement('tr');
   const trContent = `
-                <td>${Schedules.productName}</td>
-                <td>${Schedules.productNumber}</td>
-                <td>${Schedules.paymentStatus}</td>
-                <td class="${Schedules.shipping === 'Declined' ? 'danger' : Schedules.shipping === 'Pending' ? 'warning' : 'primary'}"> ${Schedules.shipping}</td>
+                <td>${schedule.productName}</td>
+                <td>${schedule.productNumber}</td>
+                <td>${schedule.paymentStatus}</td>
+                <td class="${schedule.shipping === 'Declined' ? 'danger' : schedule.shipping === 'Pending' ? 'warning' : 'primary'}"> ${schedule.shipping}</td>
                 <td class="primary"> Details </td>`;
 
-tr.innerHTML = trContent;
-document.querySelector('table tbody').appendChild(tr);
-
-
+  tr.innerHTML = trContent;
+  
+  const tbody = document.querySelector('table tbody');
+  if (tbody) {
+    tbody.appendChild(tr);
+  }
 });
+
